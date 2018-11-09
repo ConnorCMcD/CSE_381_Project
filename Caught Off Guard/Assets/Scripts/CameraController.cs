bd0888c9685951e72c4c5a29eb818cdd2473efb7
Connor McDermott <32554334+ConnorCMcD@users.noreply.github.com>
2018-11-09 18:41:18 -0500

    Camera Controls
    Basic Camera controls added
    Q/E turn left/right
    R/F turn up/down (doesn't work at straight up/down)

---
+++ Caught Off Guard/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float distance;
+    public float speed;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset.x = 0;
+        offset.y = distance / Mathf.Sqrt(2);
+        offset.z = distance / Mathf.Sqrt(2);
+    }
+
+    void FixedUpdate() {
+        float revolveHorizontal = Input.GetAxis("CameraHorizontal");
+        float revolveVertical = Input.GetAxis("CameraVertical");
+
+        offset = Quaternion.AngleAxis(speed * revolveHorizontal, Vector3.up) * offset;
+        offset = Quaternion.AngleAxis(speed * revolveVertical, transform.right) * offset;
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform);
+    }
+}
