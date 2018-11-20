2e603cc8e10aa0504caa49cdceafefd46f8891d8
Connor McDermott <32554334+ConnorCMcD@users.noreply.github.com>
2018-11-20 01:11:49 -0500

    Inventory Controls Update
    Inventory now set up to allow selection of items and descriptions of actions.  Hovering over an item in the inventory or an object in the game world will display its name and clicking an item will display "Use ___ On ___" with the first blank filled in with the item name and the second filled in the name of an object when hovered over.
    New objects will have to be added to a Dictionary in the ActionController script, which will also be in charge of taking care of interactions with that object.
    The Inventory holds a list of items which is currently hard coded to 4, with no way currently to add or remove items.  When that functionality is added, Grab and Punch are to remain hard coded as the first two "items" that can't be removed.

---
+++ Caught Off Guard/Assets/Scripts/ActionController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionController : MonoBehaviour {
+    private class COGObject
+    {
+        public string name;
+        public GameObject thing;
+
+        public COGObject(string name, GameObject thing)
+        {
+            this.name = name;
+            this.thing = thing;
+        }
+    }
+
+
+    public GameObject testblock;
+    
+    private Dictionary<string, COGObject> objects = new Dictionary<string, COGObject>();
+
+	// Use this for initialization
+	void Start () {
+        objects.Add("TEST_BLOCK", new COGObject("Block", testblock));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string GetObjectName(string objectid)
+    {
+        COGObject cobject;
+        if(objects.TryGetValue(objectid, out cobject))
+        {
+            return cobject.name;
+        }
+        return "Error";
+    }
+}
